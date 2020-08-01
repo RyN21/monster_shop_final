2735594a0969325b779fd03b1a7573db77534c01
ryn21 <33795357+RyN21@users.noreply.github.com>
2020-07-31 21:41:31 -0600

    Create relationship between merchant and discounts

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -1,5 +1,9 @@
 class Discount < ApplicationRecord
+  belongs_to :merchant
+
+  validates_presence_of :name,
+                        :description,
+                        :percent_off,
+                        :minimum_quantity
   
-
-
 end
