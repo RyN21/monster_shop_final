cbb9b37a00e7c4938b854b4471dde6f939883189
ryn21 <33795357+RyN21@users.noreply.github.com>
2020-07-31 23:23:03 -0600

    Finish story 3. Add create method to discount controller

--- app/controllers/merchant/discounts_controller.rb
+++ app/controllers/merchant/discounts_controller.rb
@@ -6,4 +6,20 @@ class Merchant::DiscountsController < Merchant::BaseController
   def new
 
   end
+
+  def create
+    merchant = current_user.merchant
+    discount = merchant.discounts.new(discount_params)
+    if discount.save
+      redirect_to '/merchant/discounts'
+    else
+      generate_flash(discount)
+      redirect_to '/merchant/discounts/new'
+    end
+  end
+
+  private
+  def discount_params
+    params.permit(:name, :description, :percent_off, :minimum_quantity)
+  end
 end
