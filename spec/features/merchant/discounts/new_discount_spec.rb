1106a7fbc68e91b10807866e7d20f8f0bba71328
ryn21 <33795357+RyN21@users.noreply.github.com>
2020-07-31 22:46:45 -0600

    Typo fix again

--- spec/features/merchant/discounts/new_discount_spec.rb
+++ spec/features/merchant/discounts/new_discount_spec.rb
@@ -25,25 +25,25 @@ RSpec.describe "Add bulk discount" do
     expect(current_path).to eq('/merchant/discounts/new')
   end
 
-  # it "can add a new discount with discount form" do
-  #   visit '/merchant/dicounts/new'
-  #
-  #   fill_in :name, with: "5% Bulk Discount"
-  #   fill_in :description, with: "Get 5% of any item when you purchase 20 or more of that item!"
-  #   fill_in :percent_off, with: 5
-  #   fill_in :minimum_quantity, with: 20
-  #
-  #   click_button "Create Discount"
-  #
-  #   new_discount = Discount.last
-  #
-  #   expect(current_path).to eq('/merchant/discounts')
-  #
-  #   within "#discount-#{new_discount.id}" do
-  #     expect(page).to have_content(new_discount.name)
-  #     expect(page).to have_content(new_discount.description)
-  #     expect(page).to have_content(new_discount.percent_off)
-  #     expect(page).to have_content(new_discount.minimum_quantity)
-  #   end
-  # end
+  it "can add a new discount with discount form" do
+    visit '/merchant/discounts/new'
+
+    fill_in :name, with: "5% Bulk Discount"
+    fill_in :description, with: "Get 5% of any item when you purchase 20 or more of that item!"
+    fill_in :percent_off, with: 5
+    fill_in :minimum_quantity, with: 20
+
+    click_button "Create Discount"
+
+    new_discount = Discount.last
+
+    expect(current_path).to eq('/merchant/discounts')
+
+    within "#discount-#{new_discount.id}" do
+      expect(page).to have_content(new_discount.name)
+      expect(page).to have_content(new_discount.description)
+      expect(page).to have_content(new_discount.percent_off)
+      expect(page).to have_content(new_discount.minimum_quantity)
+    end
+  end
 end
