9a488a92bdea3889182a80155848c149bb393f53
ryn21 <33795357+RyN21@users.noreply.github.com>
2020-08-11 13:41:43 -0600

    Rewrite routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,34 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/', to: 'welcome#index', as: :root
 
-  resources :merchants do
-    resources :items, only: [:index]
-  end
+  # resources :merchants do
+  #   resources :items, only: [:index]
+  # end
+  get '/merchants', to: 'merchants#index'
+  get '/merchants/new', to: 'merchants#new'
+  get '/merchants/:id', to: 'merchants#show', as: :merchant
+  post '/merchants', to: 'merchants#create'
+  get '/merchants/:id/edit', to: 'merchants#edit'
+  patch '/merchants/:id', to: 'merchants#update'
+  delete '/merchants/:id', to: 'merchants#destroy'
 
-  resources :items, only: [:index, :show] do
-    resources :reviews, only: [:new, :create]
-  end
+  get '/merchants/:merchant_id/items', to: 'items#index'
+  ###
 
-  resources :reviews, only: [:edit, :update, :destroy]
+  # resources :items, only: [:index, :show] do
+  #   resources :reviews, only: [:new, :create]
+  # end
+  get '/items', to: 'items#index', as: :items
+  get '/items/:item_id/reviews/new', to: 'reviews#new', as: :new_item_review
+  post '/items/:item_id/reviews', to: 'reviews#create', as: :item_reviews
+  get '/items/:id', to: 'items#show', as: :item
+  ###
+
+  # resources :reviews, only: [:edit, :update, :destroy]
+  get '/reviews/:id/edit', to: 'reviews#edit', as: :edit_review
+  patch '/reviews/:id', to: 'reviews#update', as: :review
+  delete '/reviews/:id', to: 'reviews#destroy'
+  ###
 
   get '/cart', to: 'cart#show'
   post '/cart/:item_id', to: 'cart#add_item'
@@ -18,6 +37,7 @@ Rails.application.routes.draw do
   patch '/cart/:change/:item_id', to: 'cart#update_quantity'
   delete '/cart/:item_id', to: 'cart#remove_item'
 
+  
   get '/registration', to: 'users#new', as: :registration
   resources :users, only: [:create, :update]
   patch '/user/:id', to: 'users#update'
@@ -34,30 +54,54 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#login'
   get '/logout', to: 'sessions#logout'
 
+
   get '/discounts', to: 'discounts#index'
 
   namespace :merchant do
     get '/', to: 'dashboard#index', as: :dashboard
 
-  # resources :discounts
-    get '/discounts', to: 'discounts#index', as: :discounts
-    get '/discounts/new', to: 'discounts#new'
-    post '/discounts', to: 'discounts#create'
-    get '/discounts/:id', to: 'discounts#show'
-    get '/discounts/:id/edit', to: 'discounts#edit'
-    post '/discounts/:id', to: 'discounts#update'
-    delete '/discounts/:id', to: 'discounts#destroy'
+    # get '/discounts', to: 'discounts#index', as: :discounts
+    # get '/discounts/new', to: 'discounts#new'
+    # post '/discounts', to: 'discounts#create'
+    # get '/discounts/:id', to: 'discounts#show'
+    # get '/discounts/:id/edit', to: 'discounts#edit'
+    # patch '/discounts/:id', to: 'discounts#update'
+    # delete '/discounts/:id', to: 'discounts#destroy'
+    resources :discounts
+    ###
 
-    resources :orders, only: :show
-    resources :items, only: [:index, :new, :create, :edit, :update, :destroy]
+    # resources :orders, only: :show
+    get '/orders/:id', to: 'orders#show'
+    ###
+
+
+    # resources :items, only: [:index, :new, :create, :edit, :update, :destroy]
+    get '/items', to: 'items#index'
+    get '/items/new', to: 'items#new'
+    post '/items', to: 'items#create'
+    get '/items/:id/edit', to: 'items#edit'
+    patch '/items/:id', to: 'items#update'
+    put '/items/:id', to: 'items#update'
+    delete '/items/:id', to: 'items#destroy'
+    ###
+
     put '/items/:id/change_status', to: 'items#change_status'
     get '/orders/:id/fulfill/:order_item_id', to: 'orders#fulfill'
   end
 
   namespace :admin do
     get '/', to: 'dashboard#index', as: :dashboard
-    resources :merchants, only: [:show, :update]
-    resources :users, only: [:index, :show]
+    # resources :merchants, only: [:show, :update]
+    get '/merchants/:id', to: 'merchants#show', as: :admin_merchant
+    patch '/merchants/:id', to: 'merchants#update'
+    put '/merchants/:id', to: 'merchants#update'
+    ###
+
+    # resources :users, only: [:index, :show]
+    get '/users', to: 'users#index', as: :admin_users
+    get '/users/:id', to: 'users#show', as: :admin_user
+    ###
+
     patch '/orders/:id/ship', to: 'orders#ship'
   end
 end
